c5b0fbcd44e8b86e15d1120a29e36995cf26a2bb
JD D'Annunzio <vulcancannon@gmail.com>
2022-03-09 15:39:36 -0500

    added handler functions for mouse click and key press

--- script.js
+++ script.js
@@ -2,19 +2,57 @@ const wordLength = 5;
 const keyboard = document.querySelector("[data-keyboard]");
 const gameGrid = document.querySelector("[data-gameGrid]");
 
+// remember to remove these
 console.log(keyboard);
 console.log(gameGrid);
 
 gameStart();
 
-function gameStart() 
+function gameStart()
 {
   document.addEventListener("click", handleMouseClick);
   document.addEventListener("press", handleKeyPress);
 }
 
-function gameStop() 
+function gameStop()
 {
   document.removeEventListener("click", handleMouseClick);
   document.removeEventListener("press", handleKeyPress);
+}
+
+// maybe rework these into switch cases?
+function handleMouseClick(letter)
+{
+  if (letter.target.matches("[data-letter]")) {
+    pressKey(letter.target.dataset.key);
+    return;
+  }
+
+  if (letter.target.matches("[data-enter]")) {
+    guess();
+    return;
+  }
+
+  if (letter.target.matches("[data-backspace]")) {
+    backspace();
+    return;
+  }
+}
+
+function handleKeyPress(letter)
+{
+  if (letter.key === "Enter") {
+    guess();
+    return;
+  }
+
+  if (letter.key === "Backspace" || letter.key === "Delete") {
+    backspace();
+    return;
+  }
+
+  if (letter.key.match(/^[a-z]$/)) {
+    pressKey(letter.key);
+    return;
+  }
 }
